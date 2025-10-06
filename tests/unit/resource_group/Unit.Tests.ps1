5eddb6fb87e87213e3ef430e0caf1390b89ca3c0
wesley-trust <wesley-trust@users.noreply.github.com>
2025-10-06 23:37:55 +0100

    Test [skip ci]

--- tests/unit/resource_group/Unit.Tests.ps1
+++ tests/unit/resource_group/Unit.Tests.ps1
@@ -21,6 +21,8 @@ BeforeAll {
   # Generate Bicep What-If
   $WhatIf = az deployment sub what-if --location $Location --template-file $ResourceGroupTemplateFile --parameters $ResourceGroupParameterFile --only-show-errors --no-pretty-print
 
+  #$WhatIf = Get-Content -Path "./tests/design/resource_group/bicep.whatif.json" -Raw
+
   # Create WhatIfObject if WhatIf is not null or empty, and optionally publish artifact
   if ($WhatIf) {
     if ($ENV:PUBLISHTESTARTIFACTS) {
@@ -65,7 +67,6 @@ Describe "Resource Type '<_>'" -ForEach $ResourceTypes {
 
   Context "Integrity Check" {
     It "should have at least one Resource" {
-      Write-Host $Resources
       @($Resources).Count | Should -BeGreaterThan 0
     }
   }
@@ -83,8 +84,6 @@ Describe "Resource Type '<_>'" -ForEach $ResourceTypes {
 
     BeforeAll {
       $Resource = $_
-      # Write-Host "Resources"
-      # Write-Host $WhatIfResources
       $script:WhatIfResource = $WhatIfResources | Where-Object { $_.name -eq $Resource.Name }
     }
 
@@ -100,28 +99,14 @@ Describe "Resource Type '<_>'" -ForEach $ResourceTypes {
     Context "Properties" {
       It "should have property '<_.Name>' with value '<_.Value>'" -ForEach $PropertiesObject {
         $Property = $_
-        # Write-Host "Property"
-        # Write-Host $Property
-        # Write-Host "Property Name"
-        # Write-Host $Property.Name
-        # Write-Host "Property Value"
-        # Write-Host $Property.Value
-        # Write-Host "WhatIf Resource Name"
-        # Write-Host $WhatIfResource.Name
-        # Write-Host "WhatIf Resource Value"
-        # Write-Host $WhatIfResource.Value
-        $WhatIfResource.Name | Should -BeExactly $Property.Value
+        $WhatIfResource.$($Property.Name) | Should -BeExactly $Property.Value
       }
     }
 
     Context "Tags" {
       It "should have tag '<_.Name>' with value '<_.Value>'" -ForEach $TagsObject {
         $Tag = $_
-        # Write-Host "Tag"
-        # Write-Host $Tag
-        # Write-Host "WhatIf Resource"
-        # Write-Host $WhatIfResource
-        $WhatIfResource.Tags.$Tag.Name | Should -BeExactly $Tag.Value
+        $WhatIfResource.Tags.$($Tag.Name) | Should -BeExactly $Tag.Value
       }
     }
   }
