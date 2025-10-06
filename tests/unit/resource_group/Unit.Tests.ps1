58f6e23adf98822685b5220be352551457ba13da
wesley-trust <wesley-trust@users.noreply.github.com>
2025-10-06 18:42:38 +0100

    Add unit tests for resource design integrity and structure [skip ci]

--- tests/unit/resource_group/Unit.Tests.ps1
+++ tests/unit/resource_group/Unit.Tests.ps1
@@ -1,3 +1,82 @@
-Describe 'Unit Tests' {
-  It 'passes' { $true | Should -BeTrue }
+[CmdletBinding()]
+Param(
+  [string]$DesignPath = "./tests/design/resource_group/resourcegroup.tests.json"
+)
+
+BeforeDiscovery {
+  $ErrorActionPreference = 'Stop'
+  Set-StrictMode -Version Latest
+
+  # Import Design
+  $script:Design = Get-Content -Path $DesignPath -Raw | ConvertFrom-Json
+
+  # Get unique Resource Types
+  $script:ResourceTypes = $Design.resourceType | Sort-Object -Unique
+}
+
+Describe "Resource Design" {
+  Context "Integrity Check" {
+    It "should have at least one Resource Type" {
+      $ResourceTypes.Count | Should -BeGreaterThan 0
+    }
+  }
+}
+
+Describe "Resource Type '<_>'" -ForEach $ResourceTypes {
+  $ResourceType = $_
+
+  BeforeDiscovery {
+    
+    $script:Resources = ($Design | Where-Object { $_.resourceType -eq $ResourceType }).resources
+    $Tags = ($Design | Where-Object { $_.resourceType -eq $ResourceType }).tags
+
+    $script:TagsObject = @(
+      $Tags.PSObject.Properties |
+      ForEach-Object { [pscustomobject]@{ Name = $_.Name; Value = $_.Value } }
+    )
+  }
+
+  Context "Integrity Check" {
+    It "should have at least one Resource" {
+      $Resources.Count | Should -BeGreaterThan 0
+    }
+  }
+
+  Context "Resource Name '<_.name>'" -ForEach $Resources {
+    $Resource = $_
+
+    BeforeDiscovery {
+      $script:Properties = $Resource.PSObject.Properties.Name
+    }
+
+    Context "Integrity Check" {
+      It "should have at least one Property" {
+        $Properties.Count | Should -BeGreaterThan 0
+      }
+      It "should have at least one Tag" {
+        $TagsObject.Count | Should -BeGreaterThan 0
+      }
+    }
+
+    Context "Properties" {
+      It "should have property '<_>'" -ForEach $Properties {
+        $Property = $_
+        $Property | Should -Not -BeNullOrEmpty
+      }
+    }
+
+    Context "Tags" {
+      It "should have tag '<_.Name>' with value '<_.Value>'" -ForEach $TagsObject {
+        $Tag = $_
+        $Tag.Value | Should -Not -BeNullOrEmpty
+      }
+    }
+  }
+}
+
+AfterAll {
+  $script:Properties = $null
+  $script:Resources = $null
+  $script:Design = $null
+  $script:ResourceTypes = $null
 }
